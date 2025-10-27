b7e80e4c04009a7c6555abc0ef56239b0c246693
Carlos <carloscr95r@gmail.com>
2025-10-27 18:50:40 -0300

    autoformat

--- src/app.js
+++ src/app.js
@@ -1,9 +1,10 @@
-import path from 'path';
-import express from 'express';
-import connectDB from './config/db.js';
+import path from "path";
+import express from "express";
+import connectDB from "./config/db.js";
+import config from "./config/config.js";
 
 // imports local modules
-import errorHandler from './middlewares/errorHandler.js';
+import errorHandler from "./middlewares/errorHandler.js";
 import productRoutes from "./routes/products.routes.js";
 import cartRoutes from "./routes/carts.routes.js";
 
@@ -12,16 +13,15 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/products", productRoutes);
-// app.use("/api/carts", cartRoutes);
+app.use("/api/carts", cartRoutes);
 
-app.get('/', (req, res) => {
-  res.send('Hello, World!');
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
 });
-
 
 app.use(errorHandler);
 connectDB();
 
-app.listen(process.env.PORT || 3000, () => {
-  console.log(`Server is running on http://localhost:${process.env.PORT || 3000}`);
+app.listen(config.PORT, () => {
+  console.log(`Server is running on http://localhost:${config.PORT}`);
 });
