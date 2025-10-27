0ef2b80de2a34bfdf2c3e209733948f1cfb27e06
Carlos <carloscr95r@gmail.com>
2025-10-27 18:50:30 -0300

    Prevent .env errors

--- src/config/config.js
+++ src/config/config.js
@@ -1,11 +1,11 @@
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 
 const config = {
-    PORT: process.env.PORT || 3000,
-    MONGO_URL: process.env.MONGO_URL,
-    DB_NAME: process.env.DB_NAME,
-    PERSISTENCE: process.env.PERSISTENCE || "FS"
-}
+  PORT: process.env.PORT || 3000,
+  MONGO_URL: process.env.MONGO_URL,
+  DB_NAME: process.env.DB_NAME,
+  PERSISTENCE: process.env.PERSISTENCE || "FS",
+};
 
 export default config;
--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,20 @@
-import mongoose from 'mongoose';
-import config from './config.js';
-  
+import mongoose from "mongoose";
+import config from "./config.js";
+
 const connectDB = async () => {
+  if (!config.MONGO_URL) {
+    console.error("MONGO_URL no está definida en la configuración");
+    process.exit(1); // Exit process with failure
+  }
+  if (!config.DB_NAME) {
+    console.error("DB_NAME no está definida en la configuración");
+    process.exit(1); // Exit process with failure
+  }
   try {
     await mongoose.connect(config.MONGO_URL, { dbName: config.DB_NAME });
-    console.log('Connected to MongoDB');
+    console.log("Conectado a MongoDB");
   } catch (err) {
-    console.error('Failed to connect to MongoDB', err);
+    console.error("Error al conectar a MongoDB", err);
     process.exit(1); // Exit process with failure
   }
 };
