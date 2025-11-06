c2d0d6f406847c49e04a9b4e948868d9a1d09904
Carlos <carloscr95r@gmail.com>
2025-11-06 16:44:27 -0300

    user dao

---
+++ src/dao/user.dao.js
@@ -0,0 +1,34 @@
+import User from "./models/user.model.js";
+
+class UserDao {
+    constructor(model) {
+        this.model = model;
+    }
+
+    async createUser(userData) {
+        const user = new this.model(userData);
+        return await user.save();
+    }
+
+    async getUserById(userId) {
+        return await this.model.findById(userId);
+    }
+
+    async getUserByEmail(email) {
+        return await this.model.findOne({ email });
+    }
+
+    async updateUser(userId, userData) {
+        return await this.model.findByIdAndUpdate(userId, userData, { new: true });
+    }
+
+    async deleteUser(userId) {
+        return await this.model.findByIdAndDelete(userId);
+    }
+
+    async getAllUsers() {
+        return await this.model.find();
+    }
+}
+
+export default new UserDao(User);
