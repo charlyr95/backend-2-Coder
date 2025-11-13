ecc68aaea7f09c5be1614b707321f5f41a8d55f8
Carlos <carloscr95r@gmail.com>
2025-11-12 21:28:40 -0300

    formato aplicado

--- src/controllers/session.controller.js
+++ src/controllers/session.controller.js
@@ -1,27 +1,26 @@
 import { generateToken } from "../utils/jwt.js";
- 
-export class SessionController {
 
+export class SessionController {
   static login = async (req, res, next) => {
     try {
-      if (!req.user) return res.status(401).send({message: "Credenciales inválidas"});
+      if (!req.user) return res.status(401).send({ message: "Credenciales inválidas" });
       const user = req.user;
       delete user.password;
       const accessToken = generateToken(user);
-      res.cookie("accessToken", accessToken, { httpOnly: true, maxAge: 1 * 60 * 60 * 1000 });
+      res.cookie("accessToken", accessToken, { httpOnly: true, maxAge: 1 * 60 * 60 * 1000, });
       res.status(200).send({ message: "Usuario logueado exitosamente", user });
     } catch (error) {
-      res.status(500).send({message: "Error en el login de usuario", error});
+      res.status(500).send({ message: "Error en el login de usuario", error });
     }
   };
 
   static register = async (req, res, next) => {
     try {
       // Registro manejado por Passport
-      if (!req.user) return res.status(500).send({message: "Error en el registro de usuario"});
-      res.status(201).send({message: "Usuario registrado exitosamente"});
+      if (!req.user) return res.status(500).send({ message: "Error en el registro de usuario" });
+      res.status(201).send({ message: "Usuario registrado exitosamente" });
     } catch (error) {
-      res.status(500).send({message: "Error en el registro de usuario", error});
+      res.status(500).send({ message: "Error en el registro de usuario", error });
     }
   };
 
@@ -29,10 +28,10 @@ export class SessionController {
     try {
       // Devuelve información del usuario de la sesión
       const user = req.user;
-      if (!user) return res.status(404).send({message: "Usuario no encontrado"});
+      if (!user) return res.status(404).send({ message: "Usuario no encontrado" });
       res.status(200).send(user);
     } catch (error) {
-      res.status(500).send({message: "Error al obtener usuario", error});
+      res.status(500).send({ message: "Error al obtener usuario", error });
     }
   };
 
