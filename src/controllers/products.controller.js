f13af8328dffb3612054e85945d4280e91b26ab9
Carlos <carloscr95r@gmail.com>
2025-11-30 22:10:21 -0300

    product refactor

--- src/controllers/products.controller.js
+++ src/controllers/products.controller.js
@@ -1,37 +1,36 @@
 import ProductsService from "../service/products.service.js";
 
 class ProductsController {
-
-  constructor () {
+  constructor() {
     this.service = ProductsService;
   }
 
- getProducts = async (req, res) => {
+  getProducts = async (req, res) => {
     try {
       const result = await this.service.getProducts(req.query);
       res.status(200).json(result);
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
-  }
-  
- getProductById = async (req, res) => {
+  };
+
+  getProductById = async (req, res) => {
     try {
       const result = await this.service.getProductById(req.params.pid);
       res.status(200).json(result);
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
-  }
+  };
 
- addProduct = async (req, res) => {
+  addProduct = async (req, res) => {
     try {
       const result = await this.service.addProduct(req.body);
       res.status(200).json(result);
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
-  }
+  };
 
   updateProduct = async (req, res) => {
     try {
@@ -40,7 +39,7 @@ class ProductsController {
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
-  }
+  };
 
   deleteProduct = async (req, res) => {
     try {
@@ -49,7 +48,7 @@ class ProductsController {
     } catch (err) {
       res.status(400).json({ error: err.message });
     }
-  }
+  };
 }
 
 export default new ProductsController();
--- src/service/products.service.js
+++ src/service/products.service.js
@@ -1,5 +1,5 @@
 import ProductsRepository from "../repository/products.repository.js";
-import DTO from "../dto/productsPagination.dto.js";
+import ProductsPaginationDto from "../dto/productsPagination.dto.js";
 class ProductsService {
     constructor() {
         this.repository = ProductsRepository;
@@ -7,7 +7,7 @@ class ProductsService {
 
     async getProducts({ limit = 10, page = 1, sort = null, query = null } = {}) {
         const products = await this.repository.getProducts(query ? JSON.parse(query) : {}, { limit, page, sort: sort ? JSON.parse(sort) : {} });
-        return new DTO(products);
+        return new ProductsPaginationDto(products);
     }
 
     async getProductById(id) {
@@ -32,65 +32,3 @@ class ProductsService {
 }
 
 export default new ProductsService();
-
-
-
-// class ProductsService {
-//     constructor() {
-//         this.repository = ProductsRepository;
-//     }
-
-//     async getProducts({ limit = 10, page = 1, sort, query } = {}) {
-//         try {
-//         // expected filter: {"category":"calzado","price":{"$gte":45000}}
-//         if (query && typeof query === "string") {
-//             query = JSON.parse(query);
-//         }
-//         const filter = query ? { ...query } : {};
-
-//         const sortOption = sort ? { price: sort === "asc" ? 1 : -1 } : {};
-//         const skip = (page - 1) * limit;
-
-//         const products = await model
-//             .find(filter)
-//             .sort(sortOption)
-//             .skip(skip)
-//             .limit(limit);
-//         const total = await model.countDocuments(filter);
-
-//         return {
-//             products,
-//             total,
-//             totalPages: Math.ceil(total / limit),
-//             page,
-//             limit,
-//         };
-//         } catch (error) {
-//         throw error;
-//         }
-//     }
-
-//     async getProductById(id) {
-//         return await model.findById(id);
-//     }
-
-//     async addProduct(product) {
-//         const newProduct = new model(product);
-//         return await newProduct.save();
-//     }
-
-//     async addProducts(products) {
-//         const newProducts = products.map((product) => new model(product));
-//         return await model.insertMany(newProducts);
-//     }
-
-//     async updateProduct(id, updatedFields) {
-//         return await model.findByIdAndUpdate(id, updatedFields, { new: true });
-//     }
-
-//     async deleteProduct(id) {
-//         return await model.findByIdAndDelete(id);
-//     }
-// }
-
-// export default new ProductsDao();
