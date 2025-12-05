2e2d73afc0815030c3d47fd6ea2761491eff3a2e
Carlos <carloscr95r@gmail.com>
2025-12-04 23:40:03 -0300

    added middleware

--- src/routes/carts.routes.js
+++ src/routes/carts.routes.js
@@ -2,17 +2,18 @@ import { Router } from "express";
 import controller from "../controllers/carts.controller.js";
 import { passportCall } from "../middlewares/passportCall.js";
 import authRole from "../middlewares/authRole.js";
+import { userCart } from "../middlewares/userCart.js";
 
 const router = Router();
 
 // Protected routes
-router.get("/:cid", passportCall("current"), controller.getCartById);
-router.post("/", passportCall("current"), controller.addCart);
-router.post("/:cid/product/:pid", passportCall("current"), controller.addProduct);
-router.put("/:cid", passportCall("current"), controller.updateCartProducts);
-router.delete("/:cid", passportCall("current"), controller.clearCart);
-router.put("/:cid/product/:pid", passportCall("current"), controller.updateProductQuantity);
-router.delete("/:cid/product/:pid", passportCall("current"), controller.deleteProduct);
+router.post("/", passportCall("current"), controller.addCart); 
+router.get("/:cid", passportCall("current"), userCart, controller.getCartById);
+router.post("/:cid/product/:pid", passportCall("current"), userCart, controller.addProduct);
+router.put("/:cid", passportCall("current"), userCart, controller.updateCartProducts);
+router.delete("/:cid", passportCall("current"), userCart, controller.clearCart);
+router.put("/:cid/product/:pid", passportCall("current"), userCart, controller.updateProductQuantity);
+router.delete("/:cid/product/:pid", passportCall("current"), userCart, controller.deleteProduct);
 
 // Admin route
 router.get("/", passportCall("current"), authRole("admin"), controller.getCarts);
